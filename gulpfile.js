0b8a34d6473453cfb759e410fb8518b6686331c4
Matheus Gomes <matheusgomes062@gmail.com>
2020-01-31 13:33:32 -0300

    corrections

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,33 @@
 // Adiciona os modulos instalados
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
-const concat = require('gulp-concat');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
 
 // Funçao para compilar o SASS e adicionar os prefixos
 function compilaSass() {
   return gulp
-  .src('css/scss/**/*.scss')
-  .pipe(sass({
-    outputStyle: 'compressed'
-  }))
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(gulp.dest('css/'))
-  .pipe(browserSync.stream());
+    .src("css/scss/**/*.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed"
+      })
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("css/"))
+    .pipe(browserSync.stream());
 }
 
 // Tarefa de gulp para a função de SASS
-gulp.task('sass', function(done){
+gulp.task("sass", function(done) {
   compilaSass();
   done();
 });
@@ -31,32 +35,34 @@ gulp.task('sass', function(done){
 // Função para juntar o JS
 function gulpJS() {
   return gulp
-  .src('js/main/*.js')
-  .pipe(concat('main.js'))
-  .pipe(babel({
-    presets: ['env']
-  }))
-  .pipe(uglify())
-  .pipe(gulp.dest('js/'))
-  .pipe(browserSync.stream())
+    .src("js/main/*.js")
+    .pipe(concagt("main.js"))
+    .pipe(
+      babel({
+        presets: ["env"]
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest("js/"))
+    .pipe(browserSync.stream());
 }
 
-gulp.task('mainjs', gulpJS);
+gulp.task("mainjs", gulpJS);
 
 // JS Plugins
 function pluginJS() {
   return gulp
-  .src([
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/moment/min/moment.min.js',
-    'js/plugins/*.js'
-  ])
-  .pipe(concat('plugins.js'))
-  .pipe(gulp.dest('js/'))
-  .pipe(browserSync.stream())
+    .src([
+      "node_modules/jquery/dist/jquery.min.js",
+      "node_modules/moment/min/moment.min.js",
+      "js/plugins/*.js"
+    ])
+    .pipe(concat("plugins.js"))
+    .pipe(gulp.dest("js/"))
+    .pipe(browserSync.stream());
 }
 
-gulp.task('pluginjs', pluginJS);
+gulp.task("pluginjs", pluginJS);
 
 // Função para iniciar o browser
 function browser() {
@@ -68,18 +74,21 @@ function browser() {
 }
 
 // Tarefa para iniciar o browser-sync
-gulp.task('browser-sync', browser);
+gulp.task("browser-sync", browser);
 
 // Função de watch do Gulp
 function watch() {
-  gulp.watch('css/scss/**/*.scss', compilaSass);
-  gulp.watch('js/main/*.js', gulpJS);
-  gulp.watch('js/plugins/*.js', pluginJS);
-  gulp.watch(['*.html']).on('change', browserSync.reload);
+  gulp.watch("css/scss/**/*.scss", compilaSass);
+  gulp.watch("js/main/*.js", gulpJS);
+  gulp.watch("js/plugins/*.js", pluginJS);
+  gulp.watch(["*.html"]).on("change", browserSync.reload);
 }
 
 // Inicia a tarefa de watch
-gulp.task('watch', watch);
+gulp.task("watch", watch);
 
 // Tarefa padrão do Gulp, que inicia o watch e o browser-sync
-gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'mainjs', 'pluginjs'));
+gulp.task(
+  "default",
+  gulp.parallel("watch", "browser-sync", "sass", "mainjs", "pluginjs")
+);
